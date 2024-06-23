faf3aef633a0b1dd0e87e36dffae033caf4101aa
김찬별 <47071366+cksquf98@users.noreply.github.com>
2024-06-23 16:00:25 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,50 +1,35 @@
-#재료 N개 - 각 재료의 신맛 S, 쓴맛 B
-#음식의 신맛 = 재료 신맛 곱
-#음식의 쓴맛 = 재료 쓴맛 합
+#수정사항 반영
+# 부분집합 맞는경우 바로 신맛/쓴맛 계산되도록
 
-#재료는 적어도 하나 사용, 신맛과 쓴맛의 차이가 가장 작은 요리 구하기
-
-#어 이거 부분집합인데 공집합 없도록
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-ingredient = []
-min = 100000000
 
+ingredients = []
 
-result = []
-def Subset():
-    
-    for i in range(1 << N):
-        res = []
-        for j in range(N):
-            if(i & (1 << j)):
-                res.append(ingredient[j])
-        result.append(res)
-    
+min_diff = 1000000000
 
+# 재료의 신맛과 쓴맛 입력받기
+for _ in range(N):
+    S, B = map(int, input().split())
+    ingredients.append((S, B))
 
+# 부분집합을 생성하여 신맛과 쓴맛의 차이를 계산하는 함수
+def findMin():
+    global min_diff
+    # 1부터 2^N-1 까지의 수로 부분집합을 표현 (공집합 제외)
+    for subset_mask in range(1, 1 << N):
+        total_sour = 1
+        total_bitter = 0
+        for i in range(N):
+            if subset_mask & (1 << i):  # i번째 재료가 부분집합에 포함되는 경우
+                total_sour *= ingredients[i][0]
+                total_bitter += ingredients[i][1]
+        min_diff = min(min_diff,abs(total_sour - total_bitter))
 
-for i in range(N):
-    ingredient.append(list(map(int, input().split())))
+# 최소 차이를 찾는 함수 호출
+findMin()
 
-
-Subset()
-
-
-min = 1000000000
-for igd_subset in result:
-    sin = 1
-    bitter = 0
-
-    if(len(igd_subset) == 0): continue
-    
-    for igd in igd_subset:
-        sin *= igd[0]
-        bitter += igd[1]
-    
-    if(abs(sin - bitter) < min):
-        min = abs(sin - bitter)
-
-print(min)
+# 결과 출력
+print(min_diff)
