0a421780abbf23b948f3a03610d1bff3b1d99ddf
김찬별 <47071366+cksquf98@users.noreply.github.com>
2024-08-03 22:58:09 +0900

    [Gold V] Title: 적록색약, Time: 148 ms, Memory: 115264 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,38 +1,42 @@
-def Available(row, col, color, rgPerson):
+def Available(row, col, color, normalPerson):
     if(row >= 0 and row < N) and (col >= 0 and col < N):
-        if(rgPerson):
+        if(normalPerson):
             return (visited[row][col] == False) and (color == pic[row][col])
             
 
         else:
             return (visited[row][col] == True) and (color == pic[row][col])
-    return False
+    return False  
 
 
-def BFS(rgPerson=True):
+
+def BFS(normalPerson):
     from collections import deque
 
-
-    
-    count = 0
+    ans = []
     for row in range(N):
         for col in range(N):
-            if(visited[row][col] != rgPerson):
+            if(visited[row][col] != normalPerson):
                 queue = deque()
-                queue.append([row, col])
                 color = pic[row][col]
-                count += 1
+                queue.append([row, col])
+                count = 1
                 while queue:
                     x, y = queue.popleft()
-                    visited[x][y] = rgPerson
+
+                    #일반인은 방문 시 False -> True, 적록은 True -> False로 바뀌도록
+                    visited[x][y] = normalPerson
+                    
 
                     for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                         nx, ny = x + dx, y + dy
-                        if Available(nx, ny, color, rgPerson):
+                        if Available(nx, ny, color, normalPerson):
                             queue.append((nx, ny))
-                            visited[nx][ny] = rgPerson
+                            visited[nx][ny] = normalPerson
+
+                ans.append(count)
                 
-    print(count, end = ' ')
+    print(len(ans), end = ' ')
 
 
 import sys
@@ -46,7 +50,7 @@ visited = [[False] * N for _ in range(N)]
 #일반인
 BFS(True)
 
-#적록색약
+#적록색약 체크하기 전에 pic에 있는 G를 R로 바꿔버리깅
 for i in range(N):
     for j in range(N):
         if(pic[i][j] == "G"):
