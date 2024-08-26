5486be2b08b12e91014199c4d8f644fa5ea50169
김찬별 <47071366+cksquf98@users.noreply.github.com>
2024-08-26 18:01:58 +0900

    [Gold V] Title: 물병, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.py
+++ 백준/Gold/1052. 물병/물병.py
@@ -1,12 +1,11 @@
 from math import log2
 import sys
 import heapq
-
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
 
-if N <= K:
+if(N <= K):
     print(0)
     exit()
 
@@ -17,22 +16,27 @@ while N:
     heapq.heappush(current, num)
     N -= num
 
+# print(current)
 count = 0
 
-while len(current) > K:
+while len(current) > K :
     first = heapq.heappop(current)
 
-    if not current:
+    if(not current):
         break
 
     second = current[0]
 
-    if first == second:
+
+    if(first == second):
         heapq.heappop(current)
         heapq.heappush(current, first + second)
     else:
-        # 두 배로 만들기 위해 필요한 추가 물병 수 계산
-        count += first
-        heapq.heappush(current, first * 2)
+        count += second - first
+        heapq.heappop(current)
+        heapq.heappush(current, second *2)
 
-print(count)
+if count == 0:
+    print(0)
+else:
+    print(count)
