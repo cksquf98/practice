609812417d98471020c47ea7e47931a51916f38a
김찬별 <47071366+cksquf98@users.noreply.github.com>
2025-02-01 10:07:46 +0900

    [Silver III] Title: 주유소, Time: 132 ms, Memory: 127336 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,4 +1,3 @@
-# 여기서 남은 거리까지 다 넣고 가는 비용이랑, 각자 따로 넣었을때 비용이랑 비교히면 어떨까
 import sys
 input = sys.stdin.readline
 
@@ -6,20 +5,13 @@ N = int(input())  # 도시의 개수
 distance = list(map(int, input().split(' ')))  # 연결하는 도로의 길이 = N - 1개
 oilPrice = list(map(int, input().split(' ')))  # 주유소의 리터당 가격 = N개
 
-
-price = 0
-totalDistance = 0
-for i in range(N-1):
-    price += oilPrice[i] * distance[i]
-    totalDistance += distance[i]
-
 currentPrice = 0
+currentOilPrice = oilPrice[0] # 더 싼 주유소가 있으면 갱신되도록?!
 for i in range(N-1):
-    comparePrice = currentPrice + oilPrice[i] * totalDistance
-    if comparePrice < price:
-        price = comparePrice
+    if currentOilPrice > oilPrice[i]:
+        currentPrice += oilPrice[i] * distance[i]
+        currentOilPrice = oilPrice[i]
+    else:
+        currentPrice += currentOilPrice * distance[i]
 
-    currentPrice += oilPrice[i] * distance[i]
-    totalDistance -= distance[i]
-
-print(price)
+print(currentPrice)
