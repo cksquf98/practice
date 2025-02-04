40ce3ae6666b2e786b5a0c4b988e3b3ca87f5bcc
김찬별 <47071366+cksquf98@users.noreply.github.com>
2025-02-04 10:07:58 +0900

    [Silver III] Title: 블로그, Time: 144 ms, Memory: 140084 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -1,37 +1,30 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-N, X = map(int, input().split())
-visit = list(elem for elem in map(int, input().split()))
-#덱을 써서 흠... 합산하고 앞에 애 pop -> 다음 애 push
-slide = deque()
-max_visit = 0
+# 슬라이딩 윈도우다!
+N, X = map(int, input().split(' '))
+
+visitCount = list(map(int, input().split(' ')))
+
+index = 1
+currentSum = sum(visitCount[:X])
+maxSum = currentSum
 count = 1
-for i in range(X):
-    max_visit += visit[i]
-    slide.append(visit[i])
 
-# print("start : ", max_visit)
+while index + X - 1 < N:
+    currentSum = currentSum - visitCount[index-1] + visitCount[index+X-1]
 
-current = max_visit
-for j in range(1, N-X+1):
-    current -= slide.popleft()
-    current += visit[j+X-1]
-    slide.append(visit[j+X-1])
-
-    # print("slide : ",slide)
-    # print(current)
-
-    if(current > max_visit):
-        max_visit = current
+    if currentSum > maxSum:
+        maxSum = currentSum
         count = 1
-    elif(current == max_visit):
+    elif currentSum == maxSum:
         count += 1
 
+    index += 1
 
-if(max_visit == 0):
+
+if maxSum == 0:
     print("SAD")
 else:
-    print(max_visit)
+    print(maxSum)
     print(count)
