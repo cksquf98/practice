9c4d92e2acdac23db6fbc2315fbc1743625079f5
김찬별 <47071366+cksquf98@users.noreply.github.com>
2025-03-07 23:49:32 +0900

    [Silver I] Title: 볼 모으기, Time: 128 ms, Memory: 114216 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.py
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.py
@@ -4,39 +4,30 @@ input = sys.stdin.readline
 N = int(input())  # 공의 총 개수
 
 ballList = input().rstrip()
-separateBallList = []
 
 
-def moveBall(index, color, separateBallList):
+def moveBall(index, color, ballList):
     moveCount = 0
-    for i in range(len(separateBallList)):
-        if index != i and separateBallList[i][0] == color:
-            moveCount += len(separateBallList[i])
+
+    # 0번째랑, 얘네랑 연속된 맨 앞 color 문자열들은 싹 움직일 필요가 없으니까 미리 없애놓자
+    ballList = ballList.lstrip(color)
+    for i in range(len(ballList)):
+        if index != i and ballList[i] == color:
+            moveCount += 1
     return moveCount
 
 
-ballStr = ''
-for ball in ballList:
-    if len(ballStr) > 0 and ballStr[0] == ball:
-        ballStr += ball
-    elif len(ballStr) > 0 and ballStr[0] != ball:
-        separateBallList.append(ballStr)
-        ballStr = ball
-    elif len(ballStr) == 0:
-        ballStr += ball
-separateBallList.append(ballStr)
-
 # 파란공을 왼쪽 끝으로 모아두기
-blueLeftCount = moveBall(0, 'B', separateBallList)
+blueLeftCount = moveBall(0, 'B', ballList)
 
 # 파란공을 오른쪽 끝으로 모아두기
-blueRightCount = moveBall(len(separateBallList) - 1, 'B', separateBallList)
+blueRightCount = moveBall(0, 'B', ballList[::-1])
 
 # 빨간공을 왼쪽 끝으로 모아두기
-redLeftCount = moveBall(0, 'R', separateBallList)
+redLeftCount = moveBall(0, 'R', ballList)
 
 # 빨간공을 오른쪽 끝으로 모아두기
-redRightCount = moveBall(len(separateBallList) - 1, 'R', separateBallList)
+redRightCount = moveBall(0, 'R', ballList[::-1])
 
 
 # 이동횟수가 가장 작은 애를 출력
